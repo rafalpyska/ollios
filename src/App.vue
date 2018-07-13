259bc0570f440ab5c9b8654aad095b5ffdc6f5bf
Rafał <rafix94@gmail.com>
2018-07-13 14:14:30 +0200

    Add styles to LivingRoom component (grid etc.)

--- src/App.vue
+++ src/App.vue
@@ -47,6 +47,7 @@
   .container {
     display: flex;
     width: 100%;
+    height: 100%;
     min-height: 100vh;
   }
 
--- src/components/LivingRoom.vue
+++ src/components/LivingRoom.vue
@@ -2,11 +2,11 @@
   <div class="container">
     <main class="main">
       <LoadingSpinner v-if="status"/>
-      <div class="products__container" v-for="item in dataReceived">
-        <p>{{ item.name }}</p>
-        <p>{{ item.username }}</p>
-        <p>${{ item.address.geo.lat}}</p>
-        <img :src="images" />
+      <div class="products__container" v-for="item in dataReceived" :class="'products__item--' + item.id ">
+        <img :src="images" class="products__image"/>
+        <p class="products__name">{{ item.name }}</p>
+        <p class="products__description">{{ item.company.catchPhrase }}</p>
+        <p class="products__price">${{ item.address.geo.lat}}</p>
       </div>
     </main>
   </div>
@@ -46,17 +46,40 @@
 <style scoped lang="css">
 
   .main {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    display: grid;
+    grid-template-rows: repeat(2, 50%);
+    grid-template-columns: 20% 1fr 20% 20%;
+    grid-gap: 2rem;
     width: 100%;
+    height: 100%;
+    padding: 8rem 8rem 8rem 16rem;
     background-color: rgba(240, 240, 240, 1);
   }
   .products__container {
-    display: flex;
-    flex-direction: column;
-    margin: 2rem;
     padding: 2rem;
     background-color: rgba(255, 255, 255, .9);
+  }
+  .products__image {
+    max-width: 100%;
+  }
+
+  .products__item--2 {
+    grid-column: 2/5;
+  }
+  .products__item--4 {
+    grid-column: 2/4;
+  }
+  .products__name {
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    font-weight: 300;
+  }
+  .products__description {
+    color: rgba(168, 168, 168, 1);
+    font-weight: 100;
+  }
+  .products__price {
+    color: rgba(0, 35, 255, .9);
+    font-weight: 300;
   }
 </style>
