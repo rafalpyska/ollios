92a839f9ae9668cc013e1f90114003b87cdcee49
Rafal Pyska <rafal.pyska1@gmail.com>
2021-01-24 14:35:29 +0100

    Create separate directories for client and server

---
+++ client/src/components/base/BaseLoadingSpinner.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <section class="loading">
+    <div class="spinner" :style="spinner__styles"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BaseLoadingSpinner',
+  computed: {
+    spinner__styles() {
+      return {
+        'border-radius': '100%',
+        border: '4px solid rgba(255, 255, 255, .9)',
+        'border-top': '4px solid rgba(44, 101, 255, 1)',
+        width: '100px',
+        height: '100px',
+        animation: 'spinner-animation 0.8s linear infinite'
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.loading {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--gray);
+  z-index: 12;
+}
+
+.spinner {
+  transition: all 0.3s linear;
+  margin: 0 auto;
+}
+
+@keyframes spinner-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b435385ed0f798c9225d53747f46dd8c'),
+    },
+  },
+});
